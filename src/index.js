e2172216a812cd8b7db13a468bf61e6056df187b
千风 <qianfeng.zqf@alibaba-inc.com>
2021-06-04 14:35:01 +0800

    add package.json file to src/

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 var getRawBody = require('raw-body');
 var getFormBody = require('body/form');
 var body = require('body');
-
+const { v4: uuidv4 } = require('uuid');
 
 /*
 To enable the initializer feature (https://help.aliyun.com/document_detail/156876.html)
 please implement the initializer function as below：
+*/
 exports.initializer = (context, callback) => {
-  console.log('initializing');
+  console.log('i am initializing');
   callback(null, '');
 };
-*/
+
 
 exports.handler = (req, resp, context) => {
     console.log('hello world');
 
     var params = {
+        message: 'Hello World',
         path: req.path,
         queries: req.queries,
         headers: req.headers,
         method : req.method,
         requestURI : req.url,
         clientIP : req.clientIP,
+        uuid : uuidv4()
     }
 
     getRawBody(req, function(err, body) {
